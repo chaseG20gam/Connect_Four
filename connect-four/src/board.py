896d4d32557b18441ab7627ad3d9c4503f7870ed
Adria <adria.gesse00@gmail.com>
2025-06-18 11:38:29 +0200

    added cpu easy mode to play against, logic to end the game if board full

--- connect-four/src/board.py
+++ connect-four/src/board.py
@@ -1,4 +1,7 @@
 
+
+import random
+
 class Board: # define class Board
 
     empty_cell = '.'
@@ -25,6 +28,12 @@ class Board: # define class Board
         for row in self.board:
             print(' ' + ' '.join(row)) # joins all elements in each row with a the necessary spaces for alignment
 
+    def is_full(self):  # function to check if board is fulla nd stop the game
+        for column in range(self.columns):
+            if self.board[0][column] == self.empty_cell: # look for empty cells
+                return False # theres empty cell, return false and dont stop the game
+        return True # theres NOT empty cells left, return true and stop the game
+
     def instert_piece(self, column):
         
         column = column -1 # 0-based adjustment
@@ -42,6 +51,7 @@ class Board: # define class Board
         while True: 
             try:
                 column = int(input('Select a column (0-6): ')) # user input as int
+                
                 if column == 1: # check column input
                     if self.instert_piece(column): # put the piece at the selected column
                         break # halt the loop
@@ -78,8 +88,61 @@ class Board: # define class Board
                     else:
                         print('Column is full. Try another column')
 
+                elif column == 7:
+                    if self.instert_piece(column):
+                        break
+                    else:
+                        print('Column is full. Try another column')
+
                 else:
                     print('Please enter a valid number')
 
             except ValueError: # error handliing for the input
                 print('Please enter a number')
+
+    def instert_oponent_piece(self, column):
+        
+        column = column -1 # 0-based adjustment
+        if column < 0 or column >= self.columns: # check if column is valid
+            return False
+            
+        for row in range(self.rows -1, -1, -1): # iterate from bottom to top
+            if self.board[row][column] == self.empty_cell:
+                self.board[row][column] = self.oponent_piece
+                return True
+            
+        return False # if column is full
+
+    def cpu_easy_turn(self, board):
+        
+        while True:
+            column = random.choice(range(8))
+
+            if column == 1: # check column input
+                if self.instert_oponent_piece(column): # put the piece at the selected column
+                    break # halt the loop
+
+            elif column == 2:
+                if self.instert_oponent_piece(column):
+                    break
+                
+            elif column == 3:
+                if self.instert_oponent_piece(column):
+                    break
+                
+            elif column == 4:
+                if self.instert_oponent_piece(column):
+                    break
+                
+            elif column == 5:
+                if self.instert_oponent_piece(column):
+                    break
+                
+            elif column == 6:
+                if self.instert_oponent_piece(column):
+                    break
+                
+            elif column == 7:
+                if self.instert_oponent_piece(column):
+                    break
+        
--- connect-four/src/main.py
+++ connect-four/src/main.py
@@ -2,9 +2,28 @@ from board import Board # import the board
 
 def main(): # main game loop
     board = Board()
-    while True:
+    player_turn = True
+    game_over = False
+
+    while not game_over:
         board.draw()
+        print('Your turn:')
         board.play_turn()
+        if board.is_full():
+            board.draw() # maybe a custom game over scene over the board in the future?
+            print("The board is full. It's a draw")
+            game_over = True
+            break
+
+        board.draw()
+        print("CPU's turn:")
+        board.cpu_easy_turn(board)
+        if board.is_full():
+            board.draw()
+            print("The board is full. It's a draw")
+            game_over = True
+            break
+
 
 if __name__ == "__main__":
     main()
