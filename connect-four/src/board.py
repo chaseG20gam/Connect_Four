7f5a85c42897b9f2740df52b3ad837ba918f4761
watcher <adria.gesse00@gmail.com>
2025-06-19 11:23:37 +0200

    added win check function, added a game difficuly selector. minimax algorithm and implementation next

--- connect-four/src/board.py
+++ connect-four/src/board.py
@@ -1,5 +1,3 @@
-
-
 import random
 
 class Board: # define class Board
@@ -34,7 +32,7 @@ class Board: # define class Board
                 return False # theres empty cell, return false and dont stop the game
         return True # theres NOT empty cells left, return true and stop the game
 
-    def instert_piece(self, column):
+    def insert_piece(self, column): # insert player piece
         
         column = column -1 # 0-based adjustment
         if column < 0 or column >= self.columns: # check if column is valid
@@ -46,61 +44,8 @@ class Board: # define class Board
                 return True
         
         return False # if column is full
-            
-    def play_turn(self):
-        while True: 
-            try:
-                column = int(input('Select a column (0-6): ')) # user input as int
-                
-                if column == 1: # check column input
-                    if self.instert_piece(column): # put the piece at the selected column
-                        break # halt the loop
-                    else:
-                        print('Column is full. Try another column')
-
-                elif column == 2:
-                    if self.instert_piece(column):
-                        break
-                    else:
-                        print('Column is full. Try another column')
-
-                elif column == 3:
-                    if self.instert_piece(column):
-                        break
-                    else:
-                        print('Column is full. Try another column')
-
-                elif column == 4:
-                    if self.instert_piece(column):
-                        break
-                    else:
-                        print('Column is full. Try another column')
-
-                elif column == 5:
-                    if self.instert_piece(column):
-                        break
-                    else:
-                        print('Column is full. Try another column')
-
-                elif column == 6:
-                    if self.instert_piece(column):
-                        break
-                    else:
-                        print('Column is full. Try another column')
-
-                elif column == 7:
-                    if self.instert_piece(column):
-                        break
-                    else:
-                        print('Column is full. Try another column')
-
-                else:
-                    print('Please enter a valid number')
-
-            except ValueError: # error handliing for the input
-                print('Please enter a number')
-
-    def instert_oponent_piece(self, column):
+    
+    def insert_oponent_piece(self, column): # insert rivals piece
         
         column = column -1 # 0-based adjustment
         if column < 0 or column >= self.columns: # check if column is valid
@@ -112,37 +57,133 @@ class Board: # define class Board
                 return True
             
         return False # if column is full
+    
+    def play_turn(self): # the whole logic to play a turn and place a piece 
+        while True: 
+            try:
+                column = int(input('Select a column (0-6): ')) # user input as int
+                
+                if column == 1: # check column input
+                    if self.insert_piece(column): # put the piece at the selected column
+                        break # halt the loop
+                    else:
+                        print('Column is full. Try another column')
 
-    def cpu_easy_turn(self, board):
+                elif column == 2:
+                    if self.insert_piece(column):
+                        break
+                    else:
+                        print('Column is full. Try another column')
+
+                elif column == 3:
+                    if self.insert_piece(column):
+                        break
+                    else:
+                        print('Column is full. Try another column')
+
+                elif column == 4:
+                    if self.insert_piece(column):
+                        break
+                    else:
+                        print('Column is full. Try another column')
+
+                elif column == 5:
+                    if self.insert_piece(column):
+                        break
+                    else:
+                        print('Column is full. Try another column')
+
+                elif column == 6:
+                    if self.insert_piece(column):
+                        break
+                    else:
+                        print('Column is full. Try another column')
+
+                elif column == 7:
+                    if self.insert_piece(column):
+                        break
+                    else:
+                        print('Column is full. Try another column')
+
+                else:
+                    print('Please enter a valid number')
+
+            except ValueError: # error handliing for the input
+                print('Please enter a number')
+    
+    def cpu_easy_turn(self): # easy mode to play against cpu, places a random piece at a random column
         
         while True:
             column = random.choice(range(8))
 
             if column == 1: # check column input
-                if self.instert_oponent_piece(column): # put the piece at the selected column
+                if self.insert_oponent_piece(column): # put the piece at the selected column
                     break # halt the loop
 
             elif column == 2:
-                if self.instert_oponent_piece(column):
+                if self.insert_oponent_piece(column):
                     break
                 
             elif column == 3:
-                if self.instert_oponent_piece(column):
+                if self.insert_oponent_piece(column):
                     break
                 
             elif column == 4:
-                if self.instert_oponent_piece(column):
+                if self.insert_oponent_piece(column):
                     break
                 
             elif column == 5:
-                if self.instert_oponent_piece(column):
+                if self.insert_oponent_piece(column):
                     break
                 
             elif column == 6:
-                if self.instert_oponent_piece(column):
+                if self.insert_oponent_piece(column):
                     break
                 
             elif column == 7:
-                if self.instert_oponent_piece(column):
+                if self.insert_oponent_piece(column):
                     break
-        
+
+    def cpu_normal_turn(): # cpu actually thinks whats doing, always gonna try to win or avoid you winning. (uses minimax algorithm)
+        pass
+
+    def cpu_hard_turn(): # allegedly a challenge, cpus a couple turns ahead. look for more comlpex alorithms
+        pass
+    
+    def check_win(self): # checks when four pieces of the same type are in a row and declares the winning piece (which declares a winner)
+        # check horizontal
+        for row in range(self.rows):
+            for col in range(self.columns - 3):
+                if (self.board[row][col] != '.' and
+                    self.board[row][col] == self.board[row][col + 1] == 
+                    self.board[row][col + 2] == self.board[row][col + 3]):
+                    return True, self.board[row][col]  # returns win_found(true) and winning_piece(player)
+
+        # check vertical
+        for row in range(self.rows - 3):
+            for col in range(self.columns):
+                if (self.board[row][col] != '.' and
+                    self.board[row][col] == self.board[row + 1][col] == 
+                    self.board[row + 2][col] == self.board[row + 3][col]):
+                    winner = self.board[row][col]
+                    return True, winner
+
+        # check diagonal (right to left)
+        for row in range(3, self.rows):
+            for col in range(self.columns - 3):
+                if (self.board[row][col] != '.' and
+                    self.board[row][col] == self.board[row - 1][col + 1] ==
+                    self.board[row - 2][col + 2] == self.board[row - 3][col + 3]):
+                    winner = self.board[row][col]
+                    return True, winner
+
+        # check diagonal (left ro right)
+        for row in range(self.rows - 3):
+            for col in range(self.columns - 3):
+                if (self.board[row][col] != '.' and
+                    self.board[row][col] == self.board[row + 1][col + 1] ==
+                    self.board[row + 2][col + 2] == self.board[row + 3][col + 3]):
+                    winner = self.board[row][col]
+                    return True, winner
+
+        return False, None  # no win found
--- connect-four/src/main.py
+++ connect-four/src/main.py
@@ -1,28 +1,90 @@
-from board import Board # import the board
+from board import Board # import the board settings from the file
 
 def main(): # main game loop
     board = Board()
-    player_turn = True
+    # player_turn = True
     game_over = False
+    game_diff = str(input('Choose difficulty: [easy, normal, hard]').lower) # game difficulty selection
 
-    while not game_over:
-        board.draw()
-        print('Your turn:')
-        board.play_turn()
-        if board.is_full():
-            board.draw() # maybe a custom game over scene over the board in the future?
-            print("The board is full. It's a draw")
-            game_over = True
-            break
+    if game_diff == 'easy':
+        while not game_over:
+            board.draw() # draw current board
 
-        board.draw()
-        print("CPU's turn:")
-        board.cpu_easy_turn(board)
-        if board.is_full():
+            # player turn
+            print('Your turn:')
+            board.play_turn()
+            win_found, winner = board.check_win() # two variables at one 'cause function returns two values
+
+            if win_found: # check if player won
+                board.draw() # show board state
+                if winner == board.player_piece: # checks if the piece that won is yours
+                    print('You win!')
+                else:
+                    print('CPU wins...') # conditions to win met but not your piece, cpu wins
+                game_over = True # halts the game
+                break
+
+            if board.is_full(): # check if board is full
+                board.draw() # maybe a custom game over scene over the board in the future?
+                print("The board is full. It's a draw")
+                game_over = True
+                break
+            
+            # cpu turn
+            print("CPU's turn:")
+            board.cpu_easy_turn()
+            win_found, winner = board.check_win()
+
+            if win_found: # the same process but at cpus turn
+                board.draw()
+                if winner == board.player_piece:
+                    print("You win!")
+                else:
+                    print('CPU wins...')
+                game_over = True
+                break
+
+    elif game_diff == 'Normal':
+
+        while not game_over:
             board.draw()
-            print("The board is full. It's a draw")
-            game_over = True
-            break
+            print('Your turn:')
+            board.play_turn()
+            if board.is_full():
+                board.draw() 
+                print("The board is full. It's a draw")
+                game_over = True
+                break
+
+            board.draw()
+            print("CPU's turn:")
+            board.cpu_normal_turn()
+            if board.is_full():
+                board.draw()
+                print("The board is full. It's a draw")
+                game_over = True
+                break
+
+    elif game_diff == 'Hard':
+
+        while not game_over:
+            board.draw()
+            print('Your turn:')
+            board.play_turn()
+            if board.is_full():
+                board.draw() 
+                print("The board is full. It's a draw")
+                game_over = True
+                break
+
+            board.draw()
+            print("CPU's turn:")
+            board.cpu_hard_turn()
+            if board.is_full():
+                board.draw()
+                print("The board is full. It's a draw")
+                game_over = True
+                break
 
 
 if __name__ == "__main__":
